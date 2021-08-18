cc5972aed755fd4dae234048d629829143efbd1b
hacklk <info@hackl.hu>
2021-08-18 10:46:46 +0200

    Commit Updates #03

---
+++ src/test/java/TestTest.java
@@ -0,0 +1,42 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestTest {
+
+    WebDriver webdriver;
+
+    @BeforeAll
+    public static void Init() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void SetDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+        webdriver = new ChromeDriver(options);
+
+        webdriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        webdriver.manage().window().maximize();
+    }
+
+    @AfterEach
+    public void tearDown(){
+        webdriver.quit();
+    }
+
+    @Test
+    public void goToUrl(){
+        webdriver.get("https://www.index.hu/");
+
+        Assertions.assertEquals("https://index.hu/", webdriver.getCurrentUrl());
+    }
+
+}
