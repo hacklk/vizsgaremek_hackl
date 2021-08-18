cf1a258938a1855b44088bbd0c9858bb38709470
hacklk <info@hackl.hu>
2021-08-18 10:56:21 +0200

    Commit Updates #04 TesTest

--- src/test/java/TestTest.java
+++ src/test/java/TestTest.java
@@ -3,20 +3,20 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
 import java.util.concurrent.TimeUnit;
 
 public class TestTest {
 
     WebDriver webdriver;
 
-    @BeforeAll
-    public static void Init() {
-        WebDriverManager.chromedriver().setup();
-    }
+//    @BeforeAll
+//    public static void Init() {
+//        WebDriverManager.chromedriver().setup();
+//    }
 
     @BeforeEach
     public void SetDriver() {
+        WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
